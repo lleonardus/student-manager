e7562a6a2301ce1fd49eddc350367a50a71dd997
lleonardus <leonardus.wizard@gmail.com>
2023-03-06 12:28:18 -0300

    adding constraint on student enrollment to ensure its uniqueness

--- src/main/java/com/lleonardus/studentmanager/model/Student.java
+++ src/main/java/com/lleonardus/studentmanager/model/Student.java
@@ -19,7 +19,7 @@ public class Student {
     @Column(name = "student_id")
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String enrollment;
 
     @Column(nullable = false)
